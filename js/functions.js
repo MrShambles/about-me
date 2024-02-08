68bde994b5b86ff07c277dc36504ac55cb54384a
MICHAEL ROARK <Roarkmichael4@gmail.com>
2024-02-08 12:23:16 -0800

    Big backend update, moved prompt to js file and made function, much cleaner HTML now

---
+++ js/functions.js
@@ -0,0 +1,33 @@
+
+
+function askAboutMotorcycle() {
+    let userHasRidden = confirm("Have you ridden a motorcycle?");
+    if (userHasRidden) {
+        document.write("Awesome! What's your favorite place to ride?");
+    } else {
+        document.write("What's stopping you from riding one?");
+    }
+}
+
+function whatColorIsBest() {
+    let color = prompt("What Color do you think looks best on a bike?")
+    document.write("Great Choice! " + color + " looks great on all bikes!")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
