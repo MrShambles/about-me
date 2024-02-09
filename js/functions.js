d452619fbcdcb624c577c15cc9a87d88ed4d4e21
MICHAEL ROARK <Roarkmichael4@gmail.com>
2024-02-08 17:01:47 -0800

    added loops in java for main html

--- js/functions.js
+++ js/functions.js
@@ -15,6 +15,42 @@ function whatColorIsBest() {
 }
 
 
+function rateMe() {
+    let rating = prompt("On a scale of 1-5, how good is this page? and dont lie!")
+    rating = Number(rating);
+    for( let i = 0; i < rating; i++ ){
+        document.write("🥇");
+    }
+}
+
+function triviaQuestion() {
+    let numberOfGuess = 0;
+
+    while (numberOfGuess < 3) { // Set a limit for the number of guesses
+        let answer = prompt(" **Trivia** Which American Motorcycle was created first? Indian or Harley?");
+        
+        // Convert the user's input to lowercase for case-insensitive comparison just in case
+        answer = answer.toLowerCase();
+
+        if (answer === "harley" || answer === "harley davidson") {
+            alert("Good Try! However, Harley Davidson was created in 1903! Try Again!");
+            numberOfGuess++;
+        } else if (answer === "indian") {
+            alert("Correct! Indian was created in 1901 in Springfield, MA.");
+            numberOfGuess++;
+            break;
+
+        } else {
+            alert("Invalid input. Please answer 'Indian' or 'Harley'.");
+            numberOfGuess++;
+        }
+    }
+    
+    // Inform the user if they exceeded the number of allowed guesses just in case
+    if (numberOfGuess >= 3) {
+        alert("You've exceeded the number of guesses. The correct answer is Indian (1901).");
+    }
+}
 
 
 
---
+++ js/loops.js
@@ -0,0 +1,21 @@
+// > Greater than
+// < Less Than
+// >= Greater than or equal to (gte)
+// <= Less than or equal to (lte)
+// == kinda equal to (value)
+// === totally equal to (strict - type + value)
+// (age++) changes value AFTER the code
+// (++age) changes value BEFORE the code
+// break; force stop wild loop
+
+
+
+
+
+
+
+
+
+
+
+
